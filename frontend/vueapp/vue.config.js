24609fdb7a8c2c775b3816e5405870901171728d
lc <1033581609@qq.com>
2021-04-20 16:51:57 +0800

    feat: 尝试使用gzip压缩代码，sass全局变量引入方式更改

--- frontend/vueapp/vue.config.js
+++ frontend/vueapp/vue.config.js
@@ -1,5 +1,14 @@
-const path = require('path');
-
+const CompressionWebpackPlugin = require('compression-webpack-plugin');
+const compress = new CompressionWebpackPlugin({
+  filename: info => {
+    return `${info.path}.gz${info.query}`;
+  },
+  algorithm: 'gzip',
+  threshold: 10240,
+  test: /\.js$|\.html$|\.css/,
+  minRatio: 0.8,
+  deleteOriginalAssets: false
+});
 module.exports = {
   productionSourceMap: process.env.NODE_ENV === 'development',
   css: { extract: true },
@@ -8,41 +17,57 @@ module.exports = {
       test: /\.scss$/,
       use: [
         {
-          loader: 'sass-loader'
-        },
-        {
-          loader: 'sass-resources-loader',
+          loader: 'sass-loader',
           options: {
-            resources: path.resolve(__dirname, './src/assets/css/common.scss')
+            additionalData: `@import 'src/assets/css/common.scss';`,
+            sassOptions: {
+              includePaths: [__dirname]
+            }
           }
         }
+        // {
+        //   loader: 'sass-resources-loader',
+        //   options: {
+        //     resources: path.resolve(__dirname, './src/assets/css/common.scss')
+        //   }
+        // }
       ]
     });
+    return {
+      plugins: [
+        new CompressionWebpackPlugin({
+          test: /\.js$|\.html$|\.css/, //匹配文件名
+          threshold: 10240, //对超过10k的数据进行压缩
+          deleteOriginalAssets: false //是否删除原文件
+        })
+      ]
+    };
   },
 
   chainWebpack: config => {
     config.optimization.splitChunks({
       chunks: 'all',
-      // minSize: 30000, // 形成一个新代码块最小的体积
+      minSize: 3000, // 形成一个新代码块最小的体积
       // maxAsyncRequests: 5, // 按需加载时候最大的并行请求数
       // maxInitialRequests: 3, // 最大初始化请求数
-      // automaticNameDelimiter: '~', // 打包分割符
-      // name: true,
-      // cacheGroups: {
-      //   vendor: {
-      //     name: 'vendor',
-      //     test: /[\\/]node_modules[\\/]/, //打包第三方库
-      //     chunks: 'all',
-      //     priority: 10 // 优先级
-      //   },
-      //   common: {
-      //     // 打包其余的的公共代码
-      //     minChunks: 2, // 引入两次及以上被打包
-      //     name: 'common', // 分离包的名字
-      //     chunks: 'all',
-      //     priority: 5
-      //   }
-      // }
+      automaticNameDelimiter: '~', // 打包分割符
+      name: true,
+      cacheGroups: {
+        vendor: {
+          minSize: 0,
+          name: 'vendor',
+          test: /[\\/]node_modules[\\/]/, //打包第三方库
+          chunks: 'all',
+          priority: 10 // 优先级
+        },
+        common: {
+          // 打包其余的的公共代码
+          minChunks: 2, // 引入两次及以上被打包
+          name: 'common', // 分离包的名字
+          chunks: 'all',
+          priority: 5
+        }
+      }
     });
     config.externals({
       vue: 'Vue',
